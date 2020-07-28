e459228f1935f46052fdfa9c1d847c49b341c5c0
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2020-07-28 22:18:36 +0900

    add testing library to get screenshot

---
+++ clients/react-ts-template/jest.screenshot.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: "node",
+    globalSetup: "react-screenshot-test/global-setup",
+    globalTeardown: "react-screenshot-test/global-teardown",
+    testMatch: ["**/?(*.)+(screenshot).[jt]s?(x)"],
+    transform: {
+        "^.+\\.[t|j]sx?$": "babel-jest", // or ts-jest
+        "^.+\\.css$": "react-screenshot-test/css-transform",
+        "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "react-screenshot-test/asset-transform"
+    }
+};
